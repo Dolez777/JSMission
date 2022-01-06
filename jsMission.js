8c8cc4ac73d626f30f6aea6504b7536409e80b0d
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2022-01-06 21:03:05 +0200

    Sivun säätöä, txt

--- jsMission.js
+++ jsMission.js
@@ -1,1 +1,12 @@
-let button = document.createElement("button");
+function pokemon() {
+	let xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			let answerPokemon = xmlhttp.responseText;
+			document.getElementById("rightBoxText").innerHTML = answerPokemon;
+		}
+	};
+	xmlhttp.open("GET", "ajax_info.txt", true);
+	xmlhttp.send();
+}
